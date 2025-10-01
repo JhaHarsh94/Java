928ce0000d6c89150c853ed80ca9659c62cdd080
hjha70832@gmail.com <hjha70832@gmail.com>
2025-10-01 11:13:36 +0530

    Add triangle folder

---
+++ Pattern_Printing/Triangles/Right_AngleTriangle.java
@@ -0,0 +1,20 @@
+//PATTERN
+/*  *
+    **
+    ***
+ */
+package Pattern_Printing.Triangles;
+import java.util.*;
+public class Right_AngleTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n : ");
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
