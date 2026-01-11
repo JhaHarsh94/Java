58312da15162a255656284f8f609ed49bf0a9987
hjha70832@gmail.com <hjha70832@gmail.com>
2026-01-11 14:52:08 +0530

    Linear Search

---
+++ Searching_Sorting/Linear_Search.java
@@ -0,0 +1,29 @@
+package Searching_Sorting;
+import java.util.*;
+public class Linear_Search {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the target value");
+        int x = sc.nextInt();
+        System.out.println("Enter the size of an array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements in an array");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        boolean flag = false;
+        for(int i=0; i<n; i++){
+            if(arr[i] == x){
+                flag = true;
+                break;
+            }
+        }
+        if(flag == true){
+            System.out.println("Element Found");
+        }
+        else{
+            System.out.println("Element not found");
+        }
+    }
+}
